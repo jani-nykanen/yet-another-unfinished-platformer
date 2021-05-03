31f4da18ea8e11ee822a4894cd89fed3922b8948
jani-nykanen <jnyknn@gmail.com>
2021-05-03 18:40:10 +0300

    Removing unneeded files

--- dev/editor_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="editor_tiles" tilewidth="16" tileheight="16" tilecount="32" columns="16">
- <image source="editor_tiles.png" width="256" height="32"/>
-</tileset>
